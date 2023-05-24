912404103dcf0fc9a35ae4ca13be38bea007a148
Lazar <lazvukicevic@gmail.com>
2023-05-24 01:57:27 -0400

    added links

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -4,3 +4,12 @@ from django.http import HttpResponse
 # Create your views here.
 def index(request):
     return render(request, "home/index.html")
+
+def projects(request):
+    return render(request, "home/projects.html")
+
+def resume(request):
+    return render(request, "home/resume.html")
+
+def contact(request):
+    return render(request, "home/contact.html")
